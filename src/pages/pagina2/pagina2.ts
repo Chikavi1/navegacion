aa6f0329cf632ec03344a7e4f0d4d971c958e4ae
chikavi1 <chikavi@hotmail.com>
2018-07-05 17:06:06 -0500

    agregue la pagina2

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
-
+import {Pagina3Page} from '../index.paginas';
 @Component({
   selector: 'page-pagina2',
   templateUrl: 'pagina2.html',
 })
 export class Pagina2Page {
 
+	mutantes:any[]  = [
+	{
+		nombre: "Luis Rojas",
+		ocupacion: "CEO"
+	},
+	{
+		nombre: "Olvia Diaz",
+		ocupacion: "Administracion"
+	},
+	{
+		nombre: "Juan Osorio",
+		ocupacion: "Ventas"
+	}
+	]
+
+pagina3:any = Pagina3Page;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Pagina2Page');
+  irPagina3(mutante:any){
+  	console.log(mutante);
+  	this.navCtrl.push(Pagina3Page,{ 'persona'  : mutante})
   }
-
 }
