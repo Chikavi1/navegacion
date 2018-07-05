b9f1f1dbd42b491eb3d9d300537a16f102374291
chikavi1 <chikavi@hotmail.com>
2018-07-05 16:56:24 -0500

    agregue la pagina ajustes2

--- src/pages/ajustes2/ajustes2.ts
+++ src/pages/ajustes2/ajustes2.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Ajustes2Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-ajustes2',
   templateUrl: 'ajustes2.html',
